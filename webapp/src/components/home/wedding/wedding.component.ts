e578226f871632faeac291753beed2bc5d92d009
JimmyRobz <jimmy@robz.re>
2018-01-11 19:33:29 +0100

    Complete us and wedding components.

--- webapp/src/components/home/wedding/wedding.component.ts
+++ webapp/src/components/home/wedding/wedding.component.ts
@@ -1,19 +1,38 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import * as moment from 'moment-timezone'
 
 @Component({
     selector: 'wedding',
     templateUrl: './wedding.component.html',
     styleUrls: ['./wedding.component.scss']
 })
-export class WeddingComponent {
+export class WeddingComponent implements OnInit, OnDestroy {
 
     static title = 'Mariage';
     static fragment = 'wedding';
 
-    constructor(private el: ElementRef) {
+    daysValue = '-';
+    hoursValue = '-';
+    minutesValue = '-';
+    secondsValue = '-';
+
+    interval: number;
+
+    weddingDate = moment.tz("2018-10-12 14:00", "Indian/Reunion");
+
+    ngOnInit(): void {
+        this.interval = setInterval(() => this.updateTime(), 1000)
     }
 
-    get top(): number {
-        return this.el.nativeElement.offsetTop;
+    ngOnDestroy(): void {
+        clearInterval(this.interval);
+    }
+
+    private updateTime() {
+        const duration = moment.duration(this.weddingDate.diff(moment()));
+        this.daysValue = Math.floor(duration.asDays()).toString();
+        this.hoursValue = ('0' + duration.hours()).slice(-2);
+        this.minutesValue = ('0' + duration.minutes()).slice(-2);
+        this.secondsValue = ('0' + duration.seconds()).slice(-2);
     }
 }
