4f9b530fe9623c159828fa964c2052dea7c4677f
JimmyRobz <jimmy@robz.re>
2018-01-14 23:06:09 +0100

    Complete events and pictures.

--- webapp/src/components/home/events/events.component.ts
+++ webapp/src/components/home/events/events.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector: 'events',
@@ -9,12 +10,30 @@ export class EventsComponent {
 
     static title = 'Programme';
     static fragment = 'events';
-    static active = false;
 
-    constructor(private el: ElementRef) {
+    events = [
+        {
+            picture: '/assets/images/ceremony.png',
+            title: 'La cérémonie religieuse',
+            date: 'Vendredi, 12 Octobre 2018',
+            time: '14h - 15h',
+            address: 'Église Notre Dame de la Paix - Saint-Gilles-les-Bains',
+            mapsLink: 'https://www.google.fr/maps/place/Paroisse+Notre-Dame-de-la-Paix/@-21.0559973,55.2246397,245m/data=!3m2!1e3!4b1!4m5!3m4!1s0x2182921e0f4e2065:0x869bdded5a9e0e49!8m2!3d-21.0559973!4d55.2251869'
+        },
+        {
+            picture: '/assets/images/party.jpg',
+            title: 'La soirée',
+            date: 'Vendredi, 12 Octobre 2018',
+            time: '18h - 4h',
+            address: 'Golf du Bassin Bleu - Saint-Gilles-Les-Hauts',
+            mapsLink: 'https://www.google.fr/maps/place/Golf+du+Bassin+Bleu/@-21.0534445,55.2517739,17z/data=!4m12!1m6!3m5!1s0x218291da60ce920b:0xbac58f7536eeb5d4!2sGolf+du+Bassin+Bleu!8m2!3d-21.0534445!4d55.2539626!3m4!1s0x218291da60ce920b:0xbac58f7536eeb5d4!8m2!3d-21.0534445!4d55.2539626'
+        }
+    ];
+
+    constructor(private sanitizer: DomSanitizer) {
     }
 
-    get top(): number {
-        return this.el.nativeElement.offsetTop;
+    sanitize(url) {
+        return this.sanitizer.bypassSecurityTrustUrl(url);
     }
 }
--- webapp/src/components/home/pictures/pictures.component.ts
+++ webapp/src/components/home/pictures/pictures.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector: 'pictures',
@@ -9,12 +10,30 @@ export class PicturesComponent {
 
     static title = 'Photos';
     static fragment = 'pictures';
-    static active = false;
 
-    constructor(private el: ElementRef) {
+    images: Array<Picture>;
+
+    constructor(private sanitizer: DomSanitizer) {
+        this.images = PicturesComponent.createRandomImages();
     }
 
-    get top(): number {
-        return this.el.nativeElement.offsetTop;
+    private static createRandomImages(): Array<Picture> {
+        return [[800, 600], [1024, 768], [1920, 1080], [1440, 980], [800, 600], [1440, 980]].map(size => {
+            return {
+                url: `https://picsum.photos/${size[0]}/${size[1]}/?random`,
+                width: size[0],
+                height: size[1]
+            }
+        });
     }
+
+    sanitize(url) {
+        return this.sanitizer.bypassSecurityTrustStyle(`url("${url}")`)
+    }
+}
+
+interface Picture {
+    url: string,
+    width: number,
+    height: number
 }
